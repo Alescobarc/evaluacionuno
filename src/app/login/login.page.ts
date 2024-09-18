5bfeaa875763fa88e68fb8b31c08f35047a996ec
Alexis Escobar <al.escobarc@duocuc.cl>
2024-09-18 19:07:13 -0300

    Se modifican páginas Login y Register

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  isModalOpen = false;
+  email: string = '';
+  password: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+  
+  openLoginModal() {
+    this.isModalOpen = true;
+  }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  login() {
+    if (this.email && this.password) {
+      console.log('Correo:', this.email);
+      console.log('Contraseña:', this.password);
+      this.closeModal();
+    } else {
+      console.log('Por favor ingrese correo y contraseña.');
+    }
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  isRegisterModalOpen = false;
+  fullName: string = '';
+  rut: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  openRegisterModal() {
+    this.isRegisterModalOpen = true;
+  }
+
+  // Cerrar el modal de registro
+  closeRegisterModal() {
+    this.isRegisterModalOpen = false;
+  }
+
+  // Función para registrar al usuario
+  register() {
+    if (this.fullName && this.rut && this.password && this.confirmPassword) {
+      if (this.password === this.confirmPassword) {
+        // Aquí puedes añadir la lógica de registro, como llamar a un servicio
+        console.log('Nombre:', this.fullName);
+        console.log('RUT:', this.rut);
+        console.log('Contraseña:', this.password);
+        this.closeRegisterModal();
+      } else {
+        console.log('Las contraseñas no coinciden.');
+      }
+    } else {
+      console.log('Por favor complete todos los campos.');
+    }
+  }
 }
